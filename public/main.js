6ecc74dceb76a5878b25b49d24bd8ec4258a54a9
Himanshu Gupta <himbxr22@gmail.com>
2020-10-01 15:27:46 +0530

    test1

--- public/main.js
+++ public/main.js
@@ -1,39 +1,87 @@
 // NOTE: wordSmith functions from lines 4 - 39
-// NOTE: byteSize functions from lines 41 - 76 (remember to add your API key!)
+
 
 // information to reach API
 const dataMuseUrl = 'https://api.datamuse.com/words?';
-const queryParams = 'rel_ant=';
+const queryParams1 = 'rel_ant=';
+const queryParams2 = 'rel_syn=';
+const queryParams3 = 'rel_rhy=';
 
 // selecting page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
+const responseField1 = document.querySelector('#responseField1');
+const responseField2 = document.querySelector('#responseField2');
+const responseField3 = document.querySelector('#responseField3');
 
-// AJAX function
-const getSuggestions = () => {
+// AJAX function for antonym
+const getAntonyms = () => {
   const wordQuery = inputField.value;
-  const endPoint = dataMuseUrl + queryParams + wordQuery;
+  const endPoint = dataMuseUrl + queryParams1 + wordQuery;
 
   const xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
 
   xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      renderResponse(xhr.response);
+      renderResponse1(xhr.response);
     }
   };
   xhr.open('GET', endPoint);
   xhr.send();
 }
 
+// AJAX function for synonym
+
+const getSynonyms = () => {
+  const wordQuery = inputField.value;
+  const endPoint = dataMuseUrl + queryParams2 + wordQuery;
+
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse2(xhr.response);
+    }
+  };
+  xhr.open('GET', endPoint);
+  xhr.send();
+}
+
+// AJAX function for rhym
+const getRhyming = () => {
+  const wordQuery = inputField.value;
+  const endPoint = dataMuseUrl + queryParams3 + wordQuery;
+
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse3(xhr.response);
+    }
+  };
+  xhr.open('GET', endPoint);
+  xhr.send();
+}
+
+
 // clear previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
-  while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
+  while(responseField1.firstChild){
+    responseField1.removeChild(responseField1.firstChild);
   };
-  getSuggestions();
+   while(responseField2.firstChild){
+    responseField2.removeChild(responseField2.firstChild);
+  };
+   while(responseField3.firstChild){
+    responseField3.removeChild(responseField3.firstChild);
+  };
+  getAntonyms();
+  getSynonyms();
+  getRhyming();
 };
 
 submit.addEventListener('click', displaySuggestions);
--- public/one.js
+++ public/one.js
@@ -1,30 +1,87 @@
 // Formats response to look presentable on webpage
-const renderResponse = (res) => {
+
+// for antonym
+const renderResponse1 = (res) => {
     // Handles if res is falsey
     if(!res){
       console.log(res.status);
     }
     // In case res comes back as a blank array
     if(!res.length){
-      responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
+      responseField1.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
       return;
     }
   
     // Creates an empty array to contain the HTML strings
-    let wordList = [];
+    let wordList1 = [];
     // Loops through the response and caps off at 10
     for(let i = 0; i < Math.min(res.length, 10); i++){
       // creating a list of words
-      wordList.push(`<li>${res[i].word}</li>`);
+      wordList1.push(`<li>${res[i].word}</li>`);
     }
     // Joins the array of HTML strings into one string
-    wordList = wordList.join("");
+    wordList1 = wordList1.join("");
   
     // Manipulates responseField to render the modified response
-    responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`;
+    responseField1.innerHTML = `<p>You might be interested in:</p><ol>${wordList1}</ol>`;
     return
   }
+
+  // for synonym
+  const renderResponse2 = (res) => {
   
+    if(!res){
+      console.log(res.status);
+    }
+  
+    if(!res.length){
+      responseField1.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
+      return;
+    }
+  
+  
+    let wordList2 = [];
+   
+    for(let i = 0; i < Math.min(res.length, 10); i++){
+     
+      wordList2.push(`<li>${res[i].word}</li>`);
+    }
+   
+    wordList2 = wordList2.join("");
+  
+   
+    responseField2.innerHTML = `<p>You might be interested in:</p><ol>${wordList2}</ol>`;
+    return
+  }
+// for rhyming
+  const renderResponse3 = (res) => {
+    
+    if(!res){
+      console.log(res.status);
+    }
+  
+    if(!res.length){
+      responseField3.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
+      return;
+    }
+  
+   
+    let wordList3 = [];
+   
+    for(let i = 0; i < Math.min(res.length, 10); i++){
+      // creating a list of words
+      wordList3.push(`<li>${res[i].word}</li>`);
+    }
+    // Joins the array of HTML strings into one string
+    wordList3 = wordList3.join("");
+  
+    // Manipulates responseField to render the modified response
+    responseField3.innerHTML = `<p>You might be interested in:</p><ol>${wordList3}</ol>`;
+    return;
+  }
+
+/*
+
   // Renders response before it is modified
 const renderRawResponse = (res) => {
     // Takes the first 10 words from res
@@ -32,3 +89,5 @@ const renderRawResponse = (res) => {
     // Manipulates responseField to render the unformatted response
     responseField.innerHTML = `<text>${JSON.stringify(trimmedResponse)}</text>`;
   }
+
+  */
