a822d16f433e9ff3e0c7c513777f493f25572509
Himanshu Gupta <himbxr22@gmail.com>
2020-10-01 16:09:50 +0530

    final api calls added

--- public/main.js
+++ public/main.js
@@ -9,7 +9,9 @@ const queryParams3 = 'rel_rhy=';
 
 // selecting page elements
 const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
+const submit1 = document.querySelector('#submit1');
+const submit2 = document.querySelector('#submit2');
+const submit3 = document.querySelector('#submit3');
 const responseField1 = document.querySelector('#responseField1');
 const responseField2 = document.querySelector('#responseField2');
 const responseField3 = document.querySelector('#responseField3');
@@ -68,23 +70,39 @@ const getRhyming = () => {
 
 
 // clear previous results and display results to webpage
-const displaySuggestions = (event) => {
+const displaySuggestions1 = (event) => {
   event.preventDefault();
   while(responseField1.firstChild){
     responseField1.removeChild(responseField1.firstChild);
   };
+ 
+  getAntonyms();
+
+};
+const displaySuggestions2 = (event) => {
+  event.preventDefault();
+ 
    while(responseField2.firstChild){
     responseField2.removeChild(responseField2.firstChild);
   };
+  
+ 
+  getSynonyms();
+  
+};
+const displaySuggestions3 = (event) => {
+  event.preventDefault();
+ 
    while(responseField3.firstChild){
     responseField3.removeChild(responseField3.firstChild);
   };
-  getAntonyms();
-  getSynonyms();
+ 
   getRhyming();
 };
 
-submit.addEventListener('click', displaySuggestions);
+submit1.addEventListener('click', displaySuggestions1);
+submit2.addEventListener('click', displaySuggestions2);
+submit3.addEventListener('click', displaySuggestions3);
 
 
 
